386a841586a78e8fead8878bf1e86cde887ca340
isengartz <kontokostas.thanasis@gmail.com>
2020-05-27 22:33:11 +0300

    Finished with blog animations

--- src/components/containers/Blog/Blog.js
+++ src/components/containers/Blog/Blog.js
@@ -1,17 +1,20 @@
 import React, {useEffect, useState, useRef} from "react";
-import TrackVisibility from 'react-on-screen';
 import styles from "./Blog.module.scss"
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import GlitchClip from 'react-glitch-effect/core/Clip';
 import GlitchText from 'react-glitch-effect/core/Text';
+import {TimelineMax, Power2} from "gsap/dist/gsap";
+
 const BlogSection = () => {
     const [isVisible, setIsVisible] = useState(null);
     const [seconds, setSeconds] = useState(5);
     const [started, setStarted] = useState(false);
+    const [animationStart, setAnimationStart] = useState(false);
     const [glitchIsDisabled, setGlitchIsDisabled] = useState(true);
     const visibleObserver = useRef(null);
+    let blogContainer = useRef(null);
 
     // subscribe the observer
     useEffect(() => {
@@ -33,6 +36,7 @@ const BlogSection = () => {
 
     }, [])
 
+    // If element is visible start observing
     useEffect(() => {
         const observer = visibleObserver.current;
         if (isVisible) {
@@ -51,9 +55,7 @@ const BlogSection = () => {
         if (started) {
             intervalID = setInterval(() => {
                 // when it reach 0 stop interval and start the glitches
-                console.debug(seconds);
                 if (seconds === 0 || seconds < 0) {
-                    console.debug('got inside');
                     startGlitchingAndFading();
                     clearInterval(intervalID);
                 } else {
@@ -63,18 +65,25 @@ const BlogSection = () => {
         }
         return () => clearInterval(intervalID);
     }, [started, seconds]);
+
+    // Timiline animaxtion to hide the div
+    useEffect(() => {
+        if (animationStart) {
+            const tl = new TimelineMax();
+            tl.to(blogContainer, 10, {autoAlpha: 0, ease: Power2.easeOut})
+                .to(blogContainer, 7, {height: '0px', display: "none", ease: Power2.easeOut}, "-=5")
+        }
+    }, [animationStart])
+
+    // Glitches
     const startGlitchingAndFading = () => {
-        // start the countdown
         setGlitchIsDisabled(false);
-
-        console.debug(glitchIsDisabled);
-
-        console.debug('glitching')
-        // console.debug(glitchIsDisabled);
+        setAnimationStart(true);
     }
+
     return (
-       <GlitchClip disabled={glitchIsDisabled}>
-            <Container className={styles.BlogContainer} id="blog" fluid>
+        <GlitchClip disabled={glitchIsDisabled}>
+            <Container ref={el => (blogContainer = el)} className={styles.BlogContainer} id="blog" fluid>
                 <Row>
                     <Col md={12}>
                         <h2 ref={setIsVisible} className="text-center customHeadings">Blog</h2>
@@ -85,11 +94,12 @@ const BlogSection = () => {
                         <GlitchText disabled={glitchIsDisabled} component="h4" className="text-center">
                             Did you actually think I Blog? LOL xD
                         </GlitchText>
-                        <GlitchText color1="rgba(77, 171, 245, .5)" color2="rgba(245, 0, 87, .3)" disabled={glitchIsDisabled} component="h4" className="text-center">
+                        <GlitchText color1="rgba(77, 171, 245, .5)" color2="rgba(245, 0, 87, .3)"
+                                    disabled={glitchIsDisabled} component="h4" className="text-center">
                             Just Wait For it
                         </GlitchText>
 
-                        <p className="text-center">{seconds}</p>
+                        <p className="text-center">{glitchIsDisabled ? seconds : null}</p>
                     </Col>
                 </Row>
             </Container>
