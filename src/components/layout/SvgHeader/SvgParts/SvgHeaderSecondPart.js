ecbbeb86384435b95ee325b458b12b1a8e8be533
isengartz <kontokostas.thanasis@gmail.com>
2020-06-04 19:38:49 +0300

    Broke both SVG in parts

---
+++ src/components/layout/SvgHeader/SvgParts/SvgHeaderSecondPart.js
@@ -0,0 +1,33 @@
+import React,{Fragment} from 'react';
+
+const SvgHeaderSecondPart = () => {
+    return (
+      <Fragment>
+          <g id="Layer_8">
+              <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="0" y1="437.8301" x2="720.2998"
+                              y2="437.8301">
+                  <stop offset="0.0441" stopColor="#030B26"/>
+                  <stop offset="0.2646" stopColor="#070E29"/>
+                  <stop offset="0.4912" stopColor="#131633"/>
+                  <stop offset="0.7206" stopColor="#282442"/>
+                  <stop offset="0.9506" stopColor="#453858"/>
+                  <stop offset="0.9604" stopColor="#463959"/>
+              </linearGradient>
+              <path fill="url(#SVGID_2_)" d="M720.3,417.666c0,0-13.3-17.333-60.633-5.333c-47.799,12.118-59.619,70.229-297.874,7.513
+		c-58.238-15.332-139.481-27.058-202.175-27.264C73.275,392.299,10.463,419.106,0,423.865v59.215h719.332L720.3,417.666z"/>
+          </g>
+          <g id="Layer_2">
+              <linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="0" y1="453.9492" x2="1059.0681"
+                              y2="453.9492">
+                  <stop offset="0.0441" stopColor="#030B26"/>
+                  <stop offset="1" stopColor="#363359"/>
+              </linearGradient>
+              <path fill="url(#SVGID_3_)" d="M31.477,435.701c35.768-14.617,94.574-35.057,136.256-31.362
+		c37.853,3.354,130.875,28.254,210.533,46.978c54.605,12.836,102.928,22.771,122.897,20.876
+		C603.491,462.486,688.098,402.46,720,430.854v73.145H240.793H0.514L0,449.575C0,449.575,12.494,443.458,31.477,435.701z"/>
+          </g>
+      </Fragment>
+    );
+};
+
+export default SvgHeaderSecondPart;
