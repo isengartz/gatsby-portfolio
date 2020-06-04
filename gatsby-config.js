ecfda3e7f59ca4875ff4753daa2fb2f13599b48e
isengartz <kontokostas.thanasis@gmail.com>
2020-06-04 20:16:29 +0300

    Added Gatsby Loadable SSR AND PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,20 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Sin Portfolio`,
+        short_name: `Sin Portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-loadable-components-ssr',
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
