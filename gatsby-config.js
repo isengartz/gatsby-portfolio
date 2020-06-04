72efe37ad8a29f2258576a3cd7faac74444e45d7
isengartz <kontokostas.thanasis@gmail.com>
2020-06-04 20:28:21 +0300

    Added Gatsby offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-loadable-components-ssr',
+    `gatsby-plugin-loadable-components-ssr`,
     {
       resolve: `gatsby-plugin-offline`,
       options: {
